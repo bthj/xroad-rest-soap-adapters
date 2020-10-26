0caca6bbf618916a9e3d809116e9cdf8fa8156c6
Björn Þór Jónsson <bthj@origo.is>
2020-10-26 23:46:53 +0000

    - adding / refactoring separate log scripts for Ubuntu and Red Hat - adding echo statements for the steps in the service deployment script

--- deploy-adapter-services-with-docker-compose.sh
+++ deploy-adapter-services-with-docker-compose.sh
@@ -2,18 +2,23 @@
 SCRIPTS_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$SCRIPTS_DIR" ]]; then SCRIPTS_DIR="$PWD"; fi
 
+echo "Copying REST adaptor service configuration"
 sudo cp -R $SCRIPTS_DIR/properties/rest-adapter-service /etc/
 
 # based on https://gist.github.com/mosquito/b23e1c1e5723a7fd9e6568e5cf91180f :
 
+echo "Deploying Docker Compose SystemD service configuration"
 sudo cp docker-compose@.service /etc/systemd/system/
 
+echo "Deploying xroad-rest-soap-adapters Docker Compose configuration for SystemD services"
 sudo mkdir -p /etc/docker/compose/xroad-rest-soap-adapters
-
 sudo cp $SCRIPTS_DIR/docker-compose.yaml /etc/docker/compose/xroad-rest-soap-adapters/
 
+echo "Starting docker-compose@xroad-rest-soap-adapters as a system service"
 sudo systemctl start docker-compose@xroad-rest-soap-adapters
 
+echo "Waiting 5 seconds for docker-compose@xroad-rest-soap-adapters to start"
 sleep 5
 
+echo "Listing X-Road system services:"
 sudo systemctl list-units "*xroad*"
