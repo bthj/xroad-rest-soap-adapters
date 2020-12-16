c04d4081a3ce11e157c614a873d25d5fb81ed2b9
Björn Þór Jónsson <bangsi@bthj.is>
2020-12-16 13:40:12 +0000

    - certificates generated in the deployment script - deployed certificates on the host referenced in the Docker Compose declaration

--- deploy-adapter-services-with-docker-compose.sh
+++ deploy-adapter-services-with-docker-compose.sh
@@ -5,6 +5,13 @@ if [[ ! -d "$SCRIPTS_DIR" ]]; then SCRIPTS_DIR="$PWD"; fi
 echo "Copying REST adaptor service configuration"
 sudo cp -n -R $SCRIPTS_DIR/properties/rest-adapter-service /etc/
 
+# generate a keypair for the soap adapter container to reference, via environment variables (in docker-compose)
+sudo mkdir -p /etc/xroad-soap-adapter
+sudo openssl req -nodes -new -x509 -days 7300 \ # 10 year lifetime
+  -keyout  /etc/xroad-soap-adapter/xroad-soap-adapter.key \
+  -out /etc/xroad-soap-adapter/xroad-soap-adapter.crt \
+  -subj "/C=ST/O=Local/CN=localhost"
+
 # based on https://gist.github.com/mosquito/b23e1c1e5723a7fd9e6568e5cf91180f :
 
 echo "Deploying Docker Compose SystemD service configuration"
